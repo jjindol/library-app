97e03d186f968858e4ece51ec239c36180bc443a
jjindol <andy15948@naver.com>
2023-09-22 01:34:15 +0900

    230922 commit

--- src/main/java/com/group/libraryapp/controller/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/CalculatorController.java
@@ -1,4 +1,18 @@
 package com.group.libraryapp.controller;
 
+
+import com.group.libraryapp.dto.CalculatorAddRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+
+    @GetMapping("/add") // GET /add
+    public int addTwoNumbers(CalculatorAddRequest request) {
+        return request.getNumber1() + request.getNumber2();
+    } // 객체(request) 로 감싸서 받으므로 request.을 이용해 호출해야 한다
 }
+
+// 단축키: ctrl + alt + o : import 문 정리
+// ctrl + w : 단어 영역 지정
