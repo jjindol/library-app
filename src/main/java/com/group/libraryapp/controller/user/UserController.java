4b9b3d4e9e2a2e5409ff7d2f25c4e82e65512fa7
jjindol <andy15948@naver.com>
2023-09-26 09:43:24 +0900

    230926 commit

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,18 +1,13 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.User;
 import com.group.libraryapp.dto.user.UserCreateRequest;
 import com.group.libraryapp.dto.user.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,14 +30,11 @@ public class UserController {
     @GetMapping("/user")
     public List<UserResponse> getUsers() {
         String sql = "select * from user";
-        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
-            @Override
-            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                int age = rs.getInt("age");
-                return new UserResponse(id, name, age);
-            }
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
         });
     }
 }
