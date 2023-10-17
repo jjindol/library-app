f11ee05755f473122a81cacc13f7d9b5a90fd1d6
jjindol <andy15948@naver.com>
2023-10-17 23:43:35 +0900

    231016 commit

--- src/main/java/com/group/libraryapp/service/UserService.java
+++ src/main/java/com/group/libraryapp/service/UserService.java
@@ -7,14 +7,14 @@ public class UserService { // 현재 user 의 유무 파악 및 예외 처리
 
     public void updateUser(JdbcTemplate jdbcTemplate, UserUpdateRequest request) {
 
-        String readSql = "select * from user where id = ?";
+        String readSql = "SELECT * FROM USER WHERE ID = ?";
         boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
 
         if (isUserNotExist) {
             throw new IllegalArgumentException();
         }
 
-        String sql = "update user set name = ? where id = ?"; // user 테이블에서 name 값을 특정한 값으로 업데이트
+        String sql = "UPDATE USER SET NAME = ? WHERE ID = ?"; // user 테이블에서 name 값을 특정한 값으로 업데이트
         jdbcTemplate.update(sql, request.getName(), request.getId());
     }
 }
