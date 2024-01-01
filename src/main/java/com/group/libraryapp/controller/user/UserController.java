678dda88b58108b397191bb109e9121f9add3090
Jinsu Lee <jjinsu@Jinsuui-MacBookAir.local>
2024-01-01 21:21:01 +0900

    fix: 오류 수정

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -13,47 +13,4 @@ import java.util.List;
 public class UserController {
 
 
-    private final JdbcTemplate jdbcTemplate; // 선언
-    private final UserService userService = new UserService();
-
-    public UserController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    } // 생성자
-
-
-    @PostMapping("/user") // POST /user
-    public void saveUser(@RequestBody UserCreateRequest request) {
-        String sql = "insert into user(name, age) values (?, ?)";
-        jdbcTemplate.update(sql, request.getName(), request.getAge());
-    }
-
-    @GetMapping("/user")
-    public List<UserResponse> getUsers() {
-        String sql = "select * from user";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            long id = rs.getLong("id");
-            String name = rs.getString("name");
-            int age = rs.getInt("age");
-            return new UserResponse(id, name, age);
-        });
-    }
-
-    @PutMapping("/user")
-    public void updateUser(@RequestBody UserUpdateRequest request) {
-        userService.updateUser(jdbcTemplate, request);
-    }
-
-    @DeleteMapping("/user")
-    public void deleteUser(@RequestParam String name) {
-
-        String readSql = "select * from user where name = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
-
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "delete from user where name = ?";
-        jdbcTemplate.update(sql, name);
-    }
 }
--- src/main/java/com/group/libraryapp/service/UserService.java
+++ src/main/java/com/group/libraryapp/service/UserService.java
@@ -5,16 +5,4 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserService { // 현재 user 의 유무 파악 및 예외 처리
 
-    public void updateUser(JdbcTemplate jdbcTemplate, UserUpdateRequest request) {
-
-        String readSql = "SELECT * FROM USER WHERE ID = ?";
-        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
-
-        if (isUserNotExist) {
-            throw new IllegalArgumentException();
-        }
-
-        String sql = "UPDATE USER SET NAME = ? WHERE ID = ?";
-        jdbcTemplate.update(sql, request.getName(), request.getId());
-    }
 }
