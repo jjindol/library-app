5fb9244fe1ad3ba416fd620947e7fd83504a0f1a
jjindol <andy15948@naver.com>
2024-02-02 23:55:38 +0900

    240202 commit

---
+++ src/main/java/com/group/libraryapp/controller/book/BookController.java
@@ -0,0 +1,26 @@
+package com.group.libraryapp.controller.book;
+
+import com.group.libraryapp.dto.book.BookCreateReq;
+import com.group.libraryapp.dto.book.BookLoanReq;
+import com.group.libraryapp.service.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping("/book")
+    public void saveBooks(@RequestBody BookCreateReq request) {
+        bookService.saveBook(request);
+    }
+
+    @PostMapping("/book/loan")
+    public void loanBooks(@RequestBody BookLoanReq request) {
+        bookService.loanBook(request);
+    }
+}
